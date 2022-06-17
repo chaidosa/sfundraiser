58c5064e0ad40b09241ad95d6f4c7d8c68acde7d
priteshiitdh <pritesh.verma@iitdh.ac.in>
2022-06-18 04:50:30 +0530

    Added players

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,7 @@ App = {
         petTemplate.find('.pet-breed').text(data[i].breed);
         petTemplate.find('.pet-age').text(data[i].age);
         petTemplate.find('.pet-location').text(data[i].location);
+        petTemplate.find('.pet-info').text(data[i].info)
         petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
         petsRow.append(petTemplate.html());
